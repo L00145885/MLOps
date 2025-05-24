67caa2e036df8ed76eb09acbaf3f5b8124d8871a
David McDaid <dmcdai@amazon.com>
2025-05-24 11:21:45 +0100

    Initial creation of flaskapp.py

---
+++ src/flaskapp.py
@@ -0,0 +1,14 @@
+import pickle
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Load the trained model
+model = pickle.load(open("model.pkl", "rb"))
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True,host='0.0.0.0')
